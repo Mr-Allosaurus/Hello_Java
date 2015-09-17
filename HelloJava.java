c7380df4cbd2e57b46898abd290aeae77433f02f
Mr_Allosaurus <lionpen@hotmail.com>
2015-09-17 16:07:04 +0100

    Commit test

---
+++ HelloJava.java
@@ -0,0 +1,75 @@
+/*Program: Hello Java */
+/*Brief: Prints text on the console */
+/*Date: 17/09/2015 */
+/*Version: 1.0*/ 
+/*File name: HelloJava.java */
+/*Author: Mr_Allosaurus */
+
+//------------------------------------------------------------------------
+
+/*
+DESCRIPTION
+
+Prints the String "Hello World" on the console.
+
+*/
+
+//------------------------------------------------------------------------
+
+/*
+MODIFICATIONS:
+
+N/A
+*/
+
+//------------------------------------------------------------------------
+
+/*
+INSTRUCTIONS
+
+Before you can run this program you need to compile the program by running 
+a terminal in MAC or Linux or command prompt for Microsoft Windows.
+
+1) Open a terminal/ Command prompt
+
+2) Change directory to the location of the file using the "cd /" command
+
+>> cd /DirectoryName/Folder/.../ContainingFolder >> ENTER/ RETURN
+
+3) Compile the file with the "javac" command
+
+>> javac helloWorld.java >> ENTER
+
+4) Run the program by typing java then the file name, without the file 
+extension 
+(.java) and pressing ENTER/ RETURN
+
+>> java helloWorld >> ENTER/ RETURN	
+
+*/
+
+//------------------------------------------------------------------------
+
+//PROGRAM
+
+// 1 SECTION 1
+// Print String "Hello World"
+
+class HelloJava
+{
+  public static void main (String args[]) 
+  {
+    System.out.println("Hello World!");   //Displays the enclosed String on the Screen Console
+  }
+  
+}
+
+
+// END SECTION
+
+
+//END OF PROGRAM
+
+//------------------------------------------------------------------------
+
+//END OF FILE
